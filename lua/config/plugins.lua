43e0fd4ee067dd2fd597e0216a9ddf421a2c2fb8
czrd <czrd@pop-os.localdomain>
2022-01-26 16:05:32 +0900

    add gitsigns.nvim

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -103,6 +103,14 @@ return packer.startup(function(use)
         run = ':TSUpdate',
     }
 
+    -- git
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require'gitsigns'.setup()
+        end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
