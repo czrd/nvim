31b66921d07010fd84eef63cd7e58323854d611d
Takeshi Itami <tksh.itm@gmail.com>
2022-01-27 23:41:02 +0900

    change diagnostic signs

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -1,15 +1,11 @@
 local M = {}
 
 M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  -- set diagnostic signs
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
   end
 
   local config = {
