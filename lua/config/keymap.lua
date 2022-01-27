9b1f3a5534cc36d6423e686f7d8e7180ba78b662
Takeshi Itami <tksh.itm@gmail.com>
2022-01-27 14:32:18 +0900

    format code

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,5 +1,5 @@
 local keymap = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 -- Leader key
 vim.g.mapleader = " "
@@ -20,31 +20,31 @@ keymap("n", "<C-l>", "<C-w><C-l>", opts)
 keymap("n", "<C-h>", "<C-w><C-h>", opts)
 
 -- resizing window
-keymap("n", "<C-Up>",    ":resize -2<CR>",          opts)
-keymap("n", "<C-Down>",  ":resize +2<CR>",          opts)
-keymap("n", "<C-Left>",  ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- tagbar
 keymap("n", "T", ":TagbarToggle<CR>", opts)
 
 -- tab
-keymap("n", "th", ":tabfirst<CR>",   opts)
-keymap("n", "tj", ":tabnext<CR>",    opts)
-keymap("n", "tk", ":tabprev<CR>",    opts)
-keymap("n", "tl", ":tablast<CR>",    opts)
+keymap("n", "th", ":tabfirst<CR>", opts)
+keymap("n", "tj", ":tabnext<CR>", opts)
+keymap("n", "tk", ":tabprev<CR>", opts)
+keymap("n", "tl", ":tablast<CR>", opts)
 keymap("n", "te", ":tabedit<Space>", opts)
-keymap("n", "tn", ":tabnew<Space>",  opts)
-keymap("n", "tx", ":tabclose<CR>",   opts)
+keymap("n", "tn", ":tabnew<Space>", opts)
+keymap("n", "tx", ":tabclose<CR>", opts)
 
 -- file explorer
-keymap("n", "<C-e>",      ":NvimTreeToggle<CR>",           opts)
+keymap("n", "<C-e>", ":NvimTreeToggle<CR>", opts)
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
-keymap("n", "<C-p>",      "<cmd>Telescope find_files<CR>", opts)
-keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",  opts)
+keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opts)
 
 -- navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>",     opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- indent
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,15 +1,19 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 -- install packer if needed
 if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
-        'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-        install_path
-    })
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 -- reload neovim when plugins.lua is saved
 vim.cmd([[
@@ -22,107 +26,112 @@ vim.cmd([[
 -- if failed to require packer, do nothing
 local ok, packer = pcall(require, "packer")
 if not ok then
-    print("packer is not installed")
-    return
+	print("packer is not installed")
+	return
 end
 
 return packer.startup(function(use)
-    -- packer it self
-    use 'wbthomason/packer.nvim'
+	-- packer it self
+	use("wbthomason/packer.nvim")
 
-    -- lualine
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-        config = function()
-            require('lualine').setup({
-                options = {
-                    section_separators = '', component_separators = ''
-                }
-            })
-        end,
-    }
+	-- lualine
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = {
+					section_separators = "",
+					component_separators = "",
+				},
+			})
+		end,
+	})
 
-    -- utils
-    use 'nvim-lua/plenary.nvim'
-    use 'nvim-lua/popup.nvim'
-    use 'jiangmiao/auto-pairs'
-    use {
-        'terrortylor/nvim-comment',
-        config = function() require'nvim_comment'.setup {} end
-    }
+	-- utils
+	use("nvim-lua/plenary.nvim")
+	use("nvim-lua/popup.nvim")
+	use("jiangmiao/auto-pairs")
+	use({
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup({})
+		end,
+	})
 
-    -- file navigation
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icon
-        },
-        config = function() require'nvim-tree'.setup {} end
-    }
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	-- file navigation
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- code navigation
-    use 'karb94/neoscroll.nvim'
-    use 'yuttie/comfortable-motion.vim'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use {
-        'akinsho/bufferline.nvim',
-        requires = 'kyazdani42/nvim-web-devicons',
-        config = function()
-            require('bufferline').setup()
-        end,
-    }
+	-- code navigation
+	use("karb94/neoscroll.nvim")
+	use("yuttie/comfortable-motion.vim")
+	use("lukas-reineke/indent-blankline.nvim")
+	use({
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup()
+		end,
+	})
 
-    -- tag
-    use 'craigemery/vim-autotag'
-    use 'preservim/tagbar'
+	-- tag
+	use("craigemery/vim-autotag")
+	use("preservim/tagbar")
 
-    -- terminal
-    use 'akinsho/toggleterm.nvim'
+	-- terminal
+	use("akinsho/toggleterm.nvim")
 
-    -- colorschemes
-    use 'challenger-deep-theme/vim'
-    use 'folke/tokyonight.nvim'
+	-- colorschemes
+	use("challenger-deep-theme/vim")
+	use("folke/tokyonight.nvim")
 
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
-    use 'tami5/lspsaga.nvim'
-    use 'jose-elias-alvarez/null-ls.nvim'
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
+	use("tami5/lspsaga.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
 
-    -- completion
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'saadparwaiz1/cmp_luasnip'
+	-- completion
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("saadparwaiz1/cmp_luasnip")
 
-    -- snippets
-    use 'L3MON4D3/LuaSnip'
-    use "rafamadriz/friendly-snippets"
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-    -- tree-sitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-    }
+	-- tree-sitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-    -- git
-    use {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require'gitsigns'.setup()
-        end
-    }
+	-- git
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-        require('packer').sync()
-    end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
