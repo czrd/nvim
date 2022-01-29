611fe71a0b0185bebfc210148cb758d7ceb9e7cc
Takeshi Itami <tksh.itm@gmail.com>
2022-01-29 15:51:37 +0900

    fix sumneko_lua setting

--- lua/config/lsp/settings/sumneko_lua.lua
+++ lua/config/lsp/settings/sumneko_lua.lua
@@ -4,11 +4,8 @@ return {
       diagnostics = {
         globals = { "vim" },
       },
-    },
-    workspace = {
-      library = {
-        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
       },
     },
   },
