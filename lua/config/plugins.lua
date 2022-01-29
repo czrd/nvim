ffbd9386c7b1386907f46029459dec61be2ee31e
Takeshi Itami <tksh.itm@gmail.com>
2022-01-29 16:51:09 +0900

    change file tree width

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -68,7 +68,11 @@ return packer.startup(function(use)
       "kyazdani42/nvim-web-devicons", -- optional, for file icon
     },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup {
+        view = {
+          width = 40,
+        },
+      }
     end,
   }
   use {
