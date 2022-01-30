6a5de4bb34a0b0e73ad9e69869af3fc06c8c1299
Takeshi Itami <tksh.itm@gmail.com>
2022-01-30 13:58:04 +0900

    make lualine separators triangle

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -40,12 +40,7 @@ return packer.startup(function(use)
     "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
-      require("lualine").setup {
-        options = {
-          section_separators = "",
-          component_separators = "",
-        },
-      }
+      require("lualine").setup {}
     end,
   }
 
