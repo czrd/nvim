b14b17f4c8fcd9930de043e18321a7fd5ac20c5b
Takeshi Itami <tksh.itm@gmail.com>
2022-03-16 19:51:48 +0900

    change color scheme

--- lua/config/color.lua
+++ lua/config/color.lua
@@ -1,6 +1,7 @@
-local colorscheme = "ayu"
+local colorscheme = "tokyonight"
 vim.g.vscode_style = "dark"
 vim.g.ayucolor = "dark"
+vim.g.tokyonight_style = "night"
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not ok then
