29865f8f488708c42e8bee4a0038f4591c89112f
Takeshi Itami <tksh.itm@gmail.com>
2022-06-14 21:02:16 +0900

    change keymap for nvim-tree

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -29,7 +29,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "T", ":SymbolsOutline<CR>", opts)
 
 -- file explorer
-keymap("n", "<C-e>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<C-e>", ":NvimTreeFindFileToggle<CR>", opts)
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
 keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opts)
